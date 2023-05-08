2ad4dcdad5600052d227c7c11063aa2ef59d9756
Barbara <bkmso158@gmail.com>
2023-05-08 14:07:42 +0200

    feat:adds new features

---
+++ event-handling/scripts.js
@@ -0,0 +1,29 @@
+Vue.createApp({
+  data() {
+    return {
+      x: 12,
+      y: 4,
+      fruitBasket: [
+        "🍏 Apple",
+        "🍌 Banana",
+        "🍉 Melon",
+        "🫐 Blueberry",
+        "🍓 Strawberry",
+        "🍍 Ananas",
+        "🥭 Mango",
+      ],
+    };
+  },
+  methods: {
+    removeFruit(fruitToRemove) {
+      this.fruitBasket = this.fruitBasket.filter((fruit) => {
+        return fruit !== fruitToRemove; //hier true/false => eine KOPIE von fruitBasket
+      });
+    },
+
+    updateCoordinates(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
+  },
+}).mount("#app");
--- vue-instance/script.js
+++ vue-instance/script.js
@@ -2,11 +2,6 @@ Vue.createApp({
   data() {
     return {
       userName: "John Doe",
-      //   actualTime: function () {
-      //     const event = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
-      //     timeNow = event.toLocaleString("en-GB", { timeZone: "UTC" });
-      //     return timeNow;
-      //   },
     };
   },
 }).mount("#app");
